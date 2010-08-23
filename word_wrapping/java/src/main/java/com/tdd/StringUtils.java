170d38be6a0629c3096ef4052368e0e848473760
Adam Esterline <adam@esterlines.com>
2010-08-22 19:10:45 -0600

    Solution to the word wrapping problem

--- word_wrapping/java/src/main/java/com/tdd/StringUtils.java
+++ word_wrapping/java/src/main/java/com/tdd/StringUtils.java
@@ -1,4 +1,26 @@
 package com.tdd;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public abstract class StringUtils {
+    static List<String> wrap(String input, int lineLength) {
+        if(input.length() <= lineLength) return asList(input);
+
+        String currentInput = input;
+        List<String> lines = new ArrayList<String>();
+
+        while(lineLength < currentInput.length()) {
+            String line = currentInput.substring(0, lineLength);
+            lines.add(line.trim());
+
+            currentInput = currentInput.substring(lineLength).trim();
+        }
+
+        lines.add(currentInput);
+
+        return lines;
+    }
 }
--- word_wrapping/java/src/test/java/com/tdd/WordWrappingTest.java
+++ word_wrapping/java/src/test/java/com/tdd/WordWrappingTest.java
@@ -1,4 +1,41 @@
 package com.tdd;
 
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.testng.Assert.assertEquals;
+
+@Test
 public class WordWrappingTest {
+    public void lineLengthEqualToInputStringInputShouldBeReturned() {
+        List<String> lines = StringUtils.wrap("abc", 3);
+
+        assertEquals(lines, asList("abc"));
+    }
+
+    public void inputStringLessThanRowLengthShouldReturnInputString() {
+        List<String> lines = StringUtils.wrap("abc", 6);
+
+        assertEquals(lines, asList("abc"));
+    }
+
+    public void inputStringLongerThanRowLengthShouldBeBrokenOnWordBoundry() {
+        List<String> lines = StringUtils.wrap("abc def", 3);
+
+        assertEquals(lines, asList("abc", "def"));
+    }
+
+    public void inputStringShouldSplitWordIfWordCrossesLineBoundry() {
+        List<String> lines = StringUtils.wrap("abcdef", 3);
+
+        assertEquals(lines, asList("abc", "def"));
+    }
+
+    public void inputStringShouldSplitIntoManyLinesIfTheInputStringIsLargeEnough() {
+        List<String> lines = StringUtils.wrap("abcdef abc", 3);
+
+        assertEquals(lines, asList("abc", "def", "abc"));
+    }
 }
