679e391385629cf7ec774ad456b7c4fed1ba1af2
Adam Esterline <adam@esterlines.com>
2010-08-27 21:59:23 -0600

    First shot at a word wrap solution

--- word_wrapping/ruby/lib/word_wrap.rb
+++ word_wrapping/ruby/lib/word_wrap.rb
@@ -1,2 +1,14 @@
 class WordWrap
+  def self.wrap(wrap_at, input)
+    return [input] if input.length < wrap_at
+
+    results = []
+
+    until input.empty? do
+      results << input[0, wrap_at]
+      input = (input[wrap_at..-1] || '').strip
+    end
+
+    results
+  end
 end
--- word_wrapping/ruby/test/test_word_wrap.rb
+++ word_wrapping/ruby/test/test_word_wrap.rb
@@ -2,7 +2,29 @@ require 'test/unit'
 require 'word_wrap'
 
 class TestWordWrap < Test::Unit::TestCase
-  def test_does_it_work
-    assert false 
+
+  def test_should_not_alter_a_string_shorter_than_wrap_limit
+    result = WordWrap.wrap(5, "abc")
+    assert_equal ["abc"], result
+  end
+
+  def test_should_wrap_on_word_boundries_if_possible
+    result = WordWrap.wrap(3, "abc def")
+    assert_equal ["abc", "def"], result
+  end
+
+  def test_should_wrap_in_middle_of_words
+    result = WordWrap.wrap(3, "abcdef")
+    assert_equal ["abc", "def"], result
+  end
+
+  def test_should_split_across_multiple_lines
+    result = WordWrap.wrap(3, "abcdef hij")
+    assert_equal ["abc", "def", "hij"], result
+  end
+
+  def test_should_preserve_white_space_between_word_wrapping
+    result = WordWrap.wrap(3, "a b c d e f")
+    assert_equal ["a b", "c d", "e f"], result
   end
 end
