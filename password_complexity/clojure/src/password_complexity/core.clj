2cb6237ab93388b181f8d3407c8ae54966504cb1
Adam Esterline <adam@esterlines.com>
2010-10-14 20:30:09 -0600

    first solution Clojure solution to password complexity

--- password_complexity/clojure/src/password_complexity/core.clj
+++ password_complexity/clojure/src/password_complexity/core.clj
@@ -1,1 +1,19 @@
 (ns password_complexity.core)
+
+(defn- count-passing-rules [password rules]
+  (reduce + (map #(if (% password) 1 0) rules)))
+
+(defn- make-regex-rule [re]
+  (fn [s] (not (nil? (if s (re-matches re s))))))
+
+(def contains-number? (make-regex-rule #".*[0-9]+.*"))
+(def contains-upper? (make-regex-rule #".*[A-Z]+.*"))
+(def contains-lower? (make-regex-rule #".*[a-z]+.*"))
+(def contains-special? (make-regex-rule #".*[~!@#$]+.*"))
+
+(def default-rules [contains-number? contains-upper? contains-lower? contains-special?])
+
+(defn is-complex?
+  ([password] (is-complex? password default-rules 2))
+  ([password rules required-num-rules]
+    (>= (count-passing-rules password rules) required-num-rules)))
--- password_complexity/clojure/test/password_complexity/test/core.clj
+++ password_complexity/clojure/test/password_complexity/test/core.clj
@@ -2,5 +2,46 @@
   (:use [password_complexity.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+
+(defn- always-true [_] true)
+(defn- always-false [_] false)
+
+(deftest should-be-complex-when-the-required-number-of-rules-pass
+  (is (= true (is-complex? "password" [always-true] 1)))
+  (is (= false (is-complex? "password" [always-false] 2)))
+  (is (= true (is-complex? "password" [always-false always-true] 1)))
+  (is (= false (is-complex? "password" [always-true always-false] 2))))
+
+(deftest contains-num-should-be-true-when-input-contains-at-least-one-number
+  (is (= true (contains-number? "a2b")))
+  (is (= false (contains-number? "abc")))
+  (is (= false (contains-number? "")))
+  (is (= false (contains-number? nil))))
+
+(deftest contains-upper-should-be-true-when-input-contains-at-least-one-uppercase-letter
+  (is (= true (contains-upper? "Abc")))
+  (is (= false (contains-upper? "abc")))
+  (is (= false (contains-upper? "")))
+  (is (= false (contains-upper? nil))))
+
+(deftest contains-lower-should-be-true-when-input-contains-at-least-one-lowercase-letter
+  (is (= true (contains-lower? "AbC")))
+  (is (= false (contains-lower? "ABC")))
+  (is (= false (contains-lower? "")))
+  (is (= false (contains-lower? nil))))
+
+(deftest contains-special-should-be-true-when-input-contains-at-least-one-special-character
+  (is (= true (contains-special? "~abc")))
+  (is (= true (contains-special? "a!c")))
+  (is (= true (contains-special? "a@c")))
+  (is (= true (contains-special? "#bc")))
+  (is (= true (contains-special? "ab$")))
+  (is (= false (contains-special? "abc")))
+  (is (= false (contains-special? "")))
+  (is (= false (contains-special? nil))))
+
+(deftest default-is-complex-should-require-two-default-rules
+  (is (= false (is-complex? "password")))
+  (is (= true (is-complex? "Password")))
+  (is (= true (is-complex? "p@ssword")))
+  (is (= true (is-complex? "123abc"))))
