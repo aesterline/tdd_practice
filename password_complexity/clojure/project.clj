a6235794fa9532a6a2d150714840eb976d3bf512
Adam Esterline <adam@esterlines.com>
2010-10-15 20:43:36 -0600

    converted tests to lazytest

--- password_complexity/clojure/project.clj
+++ password_complexity/clojure/project.clj
@@ -1,4 +1,7 @@
 (defproject password_complexity "1.0.0-SNAPSHOT"
   :description "A practice project solving the password complexity problem"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
+  
--- password_complexity/clojure/test/password_complexity/test/core.clj
+++ password_complexity/clojure/test/password_complexity/test/core.clj
@@ -1,52 +1,52 @@
 (ns password_complexity.test.core
-  (:use [password_complexity.core] :reload)
-  (:use [clojure.test]))
+  (:use [password_complexity.core]
+        [lazytest.describe :only (describe it)]))
 
 
 (defn- always-true [_] true)
 (defn- always-false [_] false)
 
-(deftest should-be-complex-when-the-required-number-of-rules-pass
-  (is (complex? "ab34" [always-true] 1))
-  (is (not (complex? "23ab!" [always-false] 2)))
-  (is (complex? "23rt#" [always-false always-true] 1))
-  (is (not (complex? "10wq!" [always-true always-false] 2))))
+(describe "should be complex when the required number of rules pass"
+  (it (complex? "ab34" [always-true] 1))
+  (it (not (complex? "23ab!" [always-false] 2)))
+  (it (complex? "23rt#" [always-false always-true] 1))
+  (it (not (complex? "10wq!" [always-true always-false] 2))))
 
-(deftest should-not-be-complex-when-the-password-is-a-dictionary-word
-  (is (not (complex? "password" [always-true] 1)))
-  (is (not (complex? "appLe" [always-true] 1)))
-  (is (not (complex? "TRASH" [always-true] 1))))
+(describe "should not be complex when the password is a dictionary word"
+  (it (not (complex? "password" [always-true] 1)))
+  (it (not (complex? "appLe" [always-true] 1)))
+  (it (not (complex? "TRASH" [always-true] 1))))
 
-(deftest contains-num-should-be-true-when-input-contains-at-least-one-number
-  (is (contains-number? "a2b"))
-  (is (not (contains-number? "abc")))
-  (is (not (contains-number? "")))
-  (is (not (contains-number? nil))))
+(describe "contains-number? should be true when input contains at least one number"
+  (it (contains-number? "a2b"))
+  (it (not (contains-number? "abc")))
+  (it (not (contains-number? "")))
+  (it (not (contains-number? nil))))
 
-(deftest contains-upper-should-be-true-when-input-contains-at-least-one-uppercase-letter
-  (is (contains-upper? "Abc"))
-  (is (not (contains-upper? "abc")))
-  (is (not (contains-upper? "")))
-  (is (not (contains-upper? nil))))
+(describe "contains-upper? should be true when input contains at least one uppercase letter"
+  (it (contains-upper? "Abc"))
+  (it (not (contains-upper? "abc")))
+  (it (not (contains-upper? "")))
+  (it (not (contains-upper? nil))))
 
-(deftest contains-lower-should-be-true-when-input-contains-at-least-one-lowercase-letter
-  (is (contains-lower? "AbC"))
-  (is (not (contains-lower? "ABC")))
-  (is (not (contains-lower? "")))
-  (is (not (contains-lower? nil))))
+(describe "contains-lower? should be true when input contains at least one lowercase letter"
+  (it (contains-lower? "AbC"))
+  (it (not (contains-lower? "ABC")))
+  (it (not (contains-lower? "")))
+  (it (not (contains-lower? nil))))
 
-(deftest contains-special-should-be-true-when-input-contains-at-least-one-special-character
-  (is (contains-special? "~abc"))
-  (is (contains-special? "a!c"))
-  (is (contains-special? "a@c"))
-  (is (contains-special? "#bc"))
-  (is (contains-special? "ab$"))
-  (is (not (contains-special? "abc")))
-  (is (not (contains-special? "")))
-  (is (not (contains-special? nil))))
+(describe "contains-special? should be true when input contains at least one special character"
+  (it (contains-special? "~abc"))
+  (it (contains-special? "a!c"))
+  (it (contains-special? "a@c"))
+  (it (contains-special? "#bc"))
+  (it (contains-special? "ab$"))
+  (it (not (contains-special? "abc")))
+  (it (not (contains-special? "")))
+  (it (not (contains-special? nil))))
 
-(deftest default-is-complex-should-require-two-default-rules
-  (is (not (complex? "@#$")))
-  (is (complex? "Pa$sword"))
-  (is (complex? "p@ssword"))
-  (is (complex? "123abc")))
+(describe "the default complex? should require two default-rules"
+  (it (not (complex? "@#$")))
+  (it (complex? "Pa$sword"))
+  (it (complex? "p@ssword"))
+  (it (complex? "123abc")))
