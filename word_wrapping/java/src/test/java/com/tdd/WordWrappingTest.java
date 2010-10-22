4782c838ce41e0eabb30311293fa3c0c6abf9b39
Adam Esterline <adam@esterlines.com>
2010-10-22 05:49:50 -0600

    added java 6 support to the word_wrapping pom.xml

--- word_wrapping/java/src/test/java/com/tdd/WordWrappingTest.java
+++ word_wrapping/java/src/test/java/com/tdd/WordWrappingTest.java
@@ -1,4 +1,11 @@
 package com.tdd;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+@Test
 public class WordWrappingTest {
+	public void thisTestShouldWork() {
+		assertTrue(true);
+	}
 }
