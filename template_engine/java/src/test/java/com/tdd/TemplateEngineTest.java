39ce75c3deb7990376b92a3e1bb4704b513c2c90
Adam Esterline <adam@esterlines.com>
2010-10-22 05:46:51 -0600

    add java 6 support to the template_engine pom.xml

--- template_engine/java/src/test/java/com/tdd/TemplateEngineTest.java
+++ template_engine/java/src/test/java/com/tdd/TemplateEngineTest.java
@@ -1,4 +1,11 @@
 package com.tdd;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+@Test
 public class TemplateEngineTest {
+	public void thisTestShouldPass() {
+		assertTrue(true);
+	}
 }
