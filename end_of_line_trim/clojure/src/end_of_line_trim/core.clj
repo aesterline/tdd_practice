96f24abe3daa2aa7b62a50e91fad5aef61550826
Adam Esterline <adam@esterlines.com>
2010-10-24 16:11:10 -0600

    Initial clojure end-of-line-trim

--- end_of_line_trim/clojure/src/end_of_line_trim/core.clj
+++ end_of_line_trim/clojure/src/end_of_line_trim/core.clj
@@ -1,1 +1,9 @@
-(ns end_of_line_trim.core)
+(ns end_of_line_trim.core
+  (:require [clojure.string :as str]))
+
+(defn- trim-line [match]
+  (let [[line end-of-line] (rest match)]
+    (str (str/trimr line) end-of-line)))
+
+(defn end-of-line-trim [s]
+    (apply str (map trim-line (re-seq #"(.+)([\n\r]*)" s))))
--- end_of_line_trim/clojure/test/end_of_line_trim/test/core.clj
+++ end_of_line_trim/clojure/test/end_of_line_trim/test/core.clj
@@ -2,5 +2,20 @@
   (:use [end_of_line_trim.core])
   (:use [lazytest.describe :only (describe it)]))
 
-(describe "Replace Me"
-  (it true "No tests have been written."))
+(describe "should trim whitespace from the end of the line"
+  (it (= "abc" (end-of-line-trim "abc")))
+  (it (= "abc" (end-of-line-trim "abc  ")))
+  (it (= "abc" (end-of-line-trim "abc\t")))
+  (it (= "abc\n" (end-of-line-trim "abc \n")))
+  (it (= "abc\r\n" (end-of-line-trim "abc\t\r\n"))))
+
+(describe "should not trim whitespace from the beginning of the line"
+  (it (= "  ab" (end-of-line-trim "  ab")))
+  (it (= "  ab" (end-of-line-trim "  ab  ")))
+  (it (= "  ab" (end-of-line-trim "  ab\t")))
+  (it (= "  ab\n" (end-of-line-trim "  ab  \n")))
+  (it (= "  ab\r\n" (end-of-line-trim "  ab\t  \r\n"))))
+
+(describe "should keep the original end lines"
+  (it (= "  ab\ncd" (end-of-line-trim "  ab \ncd")))
+  (it (= "ab\r\nde\n" (end-of-line-trim "ab\r\nde\n"))))
