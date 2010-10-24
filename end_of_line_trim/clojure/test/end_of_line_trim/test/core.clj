d023d7f75d649e6e3835d0b71fcb1b40e8dbba8b
Adam Esterline <adam@esterlines.com>
2010-10-23 20:44:08 -0600

    End Of Line Trim shell for clojure

---
+++ end_of_line_trim/clojure/test/end_of_line_trim/test/core.clj
@@ -0,0 +1,6 @@
+(ns end_of_line_trim.test.core
+  (:use [end_of_line_trim.core])
+  (:use [lazytest.describe :only (describe it)]))
+
+(describe "Replace Me"
+  (it true "No tests have been written."))
