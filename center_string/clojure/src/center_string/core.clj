888784dca7c6adeb8aff6c3a03f0228b41cfd29a
Adam Esterline <adam@esterlines.com>
2010-10-24 21:32:45 -0600

    first clojure implementation of center string

--- center_string/clojure/src/center_string/core.clj
+++ center_string/clojure/src/center_string/core.clj
@@ -1,1 +1,12 @@
 (ns center_string.core)
+
+(defn- spaces [len]
+  (apply str (repeat len " ")))
+
+(defn center [s len]
+  (let [slen (count s)
+        lpad (int (/ (- len slen) 2))
+        rpad (- len slen lpad)]
+    (if (<= len slen) 
+      s 
+      (str (spaces lpad) s (spaces rpad)))))
--- center_string/clojure/test/center_string/test/core.clj
+++ center_string/clojure/test/center_string/test/core.clj
@@ -2,5 +2,15 @@
   (:use [center_string.core])
   (:use [lazytest.describe :only (describe it)]))
 
-(describe "Replace Me"
-  (it true "No tests have been written."))
+(describe "should not change the string when length is not greater than the string"
+  (it (= "abc" (center "abc" 3)))
+  (it (= "abc" (center "abc" 2)))) 
+
+(describe "should distribute spaces evenly"
+  (it (= " abc " (center "abc" 5)))
+  (it (= " abcd " (center "abcd" 6)))
+  (it (= "  abc  " (center "abc" 7))))
+
+(describe "should distribute uneven spaces on the right side"
+  (it (= " abc  " (center "abc" 6)))
+  (it (= " abcd  " (center "abcd" 7))))
